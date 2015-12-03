1f993c89d839c8909af4e2b10a19aed0d8e03501
Aranza Laguna <aranzzita@gmail.com>
2015-12-03 15:11:57 -0600

    config

--- php/config.inc.php
+++ php/config.inc.php
@@ -1,16 +1,14 @@
 <?php 
-
+session_start();
  //los atributos de abajo son los que tenemos que modificar
 //$username = "$popclocdatabase"; 
 //    $password = "l7MkorqZfMw9whohAWeuzQARfKCurakHiASJTv256GkltF2itbBspaqfohks"; 
     $host = "tcp:b63ioz7h2m.database.windows.net,1433"; 
   //  $dbname = "databasepopcloc"; 
-    
-$con= array("Database"=>"databasepopcloc","Uid"=>"popcloc@b63ioz7h2m","PWD"=>"Manuel_$%&amp");
-    try 
+$con= array("Database"=>"databasepopcloc","UID"=>"popcloc@b63ioz7h2m","PWD"=>"Manuel_$%&amp");
+ try 
     { 
         $db = sqlsrv_connect($host,$con); 
-    	
 } 
     catch(PDOException $ex) 
     { 
@@ -20,29 +18,21 @@ $con= array("Database"=>"databasepopcloc","Uid"=>"popcloc@b63ioz7h2m","PWD"=>"Ma
      $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
      
-    if(function_exists('get_magic_quotes_gpc') && get_magic_quotes_gpc()) 
-    { 
-        function undo_magic_quotes_gpc(&$array) 
-        { 
-            foreach($array as &$value) 
-            { 
-                if(is_array($value)) 
-                { 
-                    undo_magic_quotes_gpc($value); 
-                } 
-                else 
-                { 
-                    $value = stripslashes($value); 
-                } 
-            } 
-        } 
-     
-        undo_magic_quotes_gpc($_POST); 
-        undo_magic_quotes_gpc($_GET); 
-        undo_magic_quotes_gpc($_COOKIE); 
-    } 
-    header('Content-Type: text/html; charset=utf-8'); 
-    session_start(); 
-
-
+    $correo = $_REQUEST['correo'];
+    $contraseña = $_REQUEST['contraseña'];
+    $consulta = "SELECT 
+                *
+            FROM usuarios 
+            WHERE 
+                correo = 'correo'  AND contraseña = 'contraseña'";
+    $res = sqlsrv_query($db , $consulta, array(), array( "Scrollable" => SQLSRV_CURSOR_KEYSET ));
+    if($res == true){
+    $_SESSION['valid_user'] = true;
+    $_SESSION['correo'] = $correo;
+    header('Location: ../index.php');
+    die();
+}else{
+    header('Location: error.html');
+    die();
+}
 ?>
