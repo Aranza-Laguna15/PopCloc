bef39478354270c996a65ddb1ec5b73c3005ea33
Aranza Laguna <aranzzita@gmail.com>
2015-12-03 23:26:16 -0600

    config

--- php/config.inc.php
+++ php/config.inc.php
@@ -6,25 +6,17 @@ session_start();
     $host = "tcp:b63ioz7h2m.database.windows.net,1433"; 
   //  $dbname = "databasepopcloc"; 
 $con= array("Database"=>"databasepopcloc","UID"=>"popcloc@b63ioz7h2m","PWD"=>"Manuel_$%&amp");
- try 
-    { 
-        $db = sqlsrv_connect($host,$con); 
-} 
-    catch(PDOException $ex) 
-    { 
-        die("Error al conectar la base de datos: " . $ex->getMessage()); 
-    } 
-     
+ $db = sqlsrv_connect($host,$con);
+ if($db == true){
+  echo "Error al conectar la base de datos\n";
+  die(print_r( sqlsrv_errors(), true));
+ }
      $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
      
     $correo = $_REQUEST['correo'];
     $contraseña = $_REQUEST['contraseña'];
-    $consulta = "SELECT 
-                *
-            FROM usuarios 
-            WHERE 
-                correo = 'correo'  AND contraseña = 'contraseña'";
+    $consulta = "SELECT * FROM usuarios WHERE correo = 'correo'  AND contraseña = 'contraseña'";
     $res = sqlsrv_query($db , $consulta, array(), array( "Scrollable" => SQLSRV_CURSOR_KEYSET ));
     if($res == true){
     $_SESSION['valid_user'] = true;
