118cd092ae22e620aeeb7d46d82925245837387a
Aranza Laguna <aranzzita@gmail.com>
2015-12-04 21:53:43 -0600

    config

--- php/config.inc.php
+++ php/config.inc.php
@@ -1,23 +1,26 @@
 <?php 
 session_start();
-$username = "popcloc@b63ioz7h2m"; 
-    $password = "Manuel_$%&"; 
+$username = 'popcloc@b63ioz7h2m'; 
+    $password = 'Manuel_$%&'; 
     $host = "tcp:b63ioz7h2m.database.windows.net,1433"; 
-    $dbname = "databasepopcloc"; 
-$db = new PDO("sqlsrv:server=$host,Database=$dbname", $username, $password); 
- if($db){
+    $dbname = 'databasepopcloc'; 
+    $connectinfo = array("Database"=>$dbname, "UID"=>$username, "PWD"=>$password, "MultipleActiveResultSets"=>true);
+    sqlsrv_configure('WarningsReturnAsErrors',0);
+//$db = new PDO("sqlsrv:server=$host,Database=$dbname", $username, $password);
+$con = sqlsrv_connect($host, $connectinfo);
+ if($con == true){
   alert('Conexión establecida');
  }else{
  alert('Error al conectar la base de datos\n');
   die(print_r( sqlsrv_errors(), true));
  }
-     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
-     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+     //$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+     //$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
      
     $correo = $_REQUEST['nombreusuario'];
     $contraseña = $_REQUEST['contraseña'];
     $consulta = "SELECT * FROM usuarios WHERE correo = 'nombreusuario'  AND contraseña = 'contraseña'";
-    $res = sqlsrv_query($db , $consulta, array(), array( "Scrollable" => SQLSRV_CURSOR_KEYSET ));
+    $res = sqlsrv_query($con , $consulta);
     if($res == true){
     $_SESSION['valid_user'] = true;
     $_SESSION['nombreusuario'] = $correo;
@@ -25,6 +28,6 @@ $db = new PDO("sqlsrv:server=$host,Database=$dbname", $username, $password);
     die();
 }else{
     header('Location: ../index.php');
-    die();
+    die(print_r( sqlsrv_errors(), true));
 }
 ?>
